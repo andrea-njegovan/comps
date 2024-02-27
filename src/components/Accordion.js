d0736b92ab9987308c2cbd858de293be5ee5e488
Andrea Njegovan <andreanjegovan@gmail.com>
2024-02-27 12:30:57 +0100

    accordion - handleClick with current value, icon

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,44 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-	const [expanedIndex, setExpandedIndex] = useState(1);
+	const [expanedIndex, setExpandedIndex] = useState(-1);
 
 	const renderedItems = items.map((item, index) => {
 		const isExpanded = index === expanedIndex;
 
+		const handleClick = (nextIndex) => {
+			// Get the latest value in the queue
+			setExpandedIndex((currentExpandedIndex) => {
+				if (expanedIndex === nextIndex) {
+					return -1;
+				} else {
+					return nextIndex;
+				}
+			});
+		};
+
+		const icon = (
+			<span className="text-2xl">
+				{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+			</span>
+		);
+
 		return (
 			<div key={item.id}>
-				<div>{item.label}</div>
-				{isExpanded && <div>{item.content}</div>}
+				<div
+					className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+					onClick={() => handleClick(index)}
+				>
+					{item.label}
+					{icon}
+				</div>
+				{isExpanded && <div className="border-b p-5">{item.content}</div>}
 			</div>
 		);
 	});
 
-	return <div>{renderedItems}</div>;
+	return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
